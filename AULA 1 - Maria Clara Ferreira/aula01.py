30d5f2072601ea59a75cd68dd3ebdd30d8c0fa86
clarinha024 <mariiacfrodrigues@gmail.com>
2024-12-06 09:47:35 -0300

    Add files via upload
    O Carlos divo ensinou agnt muito bem hoje

---
+++ AULA 1 - Maria Clara Ferreira/aula01.py
@@ -0,0 +1,6 @@
+print('Hola Mundo$') 
+print('Maria Rainha e o resto nadinha') 
+print('VAI CORINTHIANS')
+print ('completamente louco mas um louco consciente')
+print('Eu tenho habilidade de fazer historias tristes virarem melodia')
+print('Não dá pra fraquejar, quem é guerreiro ligado que guerreiro é assim')
